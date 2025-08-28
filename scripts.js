2747036a6e62296d3ea926d68c2f7aa5057b4c74
ClemerGheno97 <clemergheno97@gmail.com>
2025-08-27 22:46:11 -0300

    Primeiro Commit - Conversor de Moedas

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const convertButton = document.querySelector(".convert-button")
+const moedaSelect = document.querySelector(".moeda-select")
+
+function convertValues() {
+    const inputValor = document.querySelector(".input-valor").value
+    const moedaConverter = document.querySelector(".valor-section")
+    const moedaConvertida = document.querySelector(".valor-section-convertido")
+
+    console.log(moedaSelect.value)
+    const dolarToday = 5.20
+    const euroToday = 6.20
+    const libraToday = 3.20
+
+    moedaConverter.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputValor)
+
+    if (moedaSelect.value == "dolar") {
+        moedaConvertida.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputValor / dolarToday)
+    }
+
+    if (moedaSelect.value == "euro") {
+        moedaConvertida.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputValor / euroToday)
+    }
+
+    if (moedaSelect.value == "libra") {
+        moedaConvertida.innerHTML = new Intl.NumberFormat('en-GB', {
+            style: 'currency',
+            currency: 'GBP'
+        }).format(inputValor / libraToday)
+    }
+
+}
+
+convertButton.addEventListener("click", convertValues)
